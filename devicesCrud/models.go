75bb15e43776ec7c8a42edbb6198d473969ddc95
Emmanuel Bastidas <82973807+ank634@users.noreply.github.com>
2025-07-21 16:20:29 -0700

    update structs to use pointers so now some values can be optional and not zero value initialized if not present in user sent json

--- devicesCrud/models.go
+++ devicesCrud/models.go
@@ -1,16 +1,39 @@
 package devicesCrud
 
 type SmartHomeDevice struct {
-	DeviceID    string
-	DeviceName  string
-	DeviceType  string
-	ServiceType string
-	SetTopic    string
-	GetTopic    string
-	DeviceUrl   string
+	DeviceID    *string
+	DeviceName  *string
+	DeviceType  *string
+	ServiceType *string
+	Manufactor  *string
+	SetTopic    *string
+	GetTopic    *string
+	DeviceUrl   *string
+}
+
+type LightDevice struct {
+	DeviceID    *string
+	DeviceName  *string
+	DeviceType  *string
+	ServiceType *string
+	Manufactor  *string
+	SetTopic    *string
+	GetTopic    *string
+	EndPoint    *string
+	RoomID      *int
+
+	IsDimmable *bool
+	IsRgb      *bool
 }
 
 type SmartHomeDevicePatch struct {
 	DeviceID   string
 	DeviceName string
+}
+
+type ProblemDetail struct {
+	ErrorType string
+	Title     string
+	Status    int
+	Detail    string
 }
